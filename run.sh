27a7056c706c0ae1c77cb005d8af43c00f5888ee
王俊 <102752306+dan211505017@users.noreply.github.com>
2025-06-07 19:34:51 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,11 +2,8 @@ today=`date -u "+%Y-%m-%d"`
 cd daily_arxiv
 scrapy crawl arxiv -o ../data/${today}.jsonl
 
-cd ../ai
-python enhance.py --data ../data/${today}.jsonl
-
 cd ../to_md
-python convert.py --data ../data/${today}_AI_enhanced_${LANGUAGE}.jsonl
+python convert.py --data ../data/${today}.jsonl
 
 cd ..
 python create_markdown.py data/${today}.jsonl
